07872f294e5d15b9ba58a012edff4f015309a5c3
papa0four <papa.o.deuce@gmail.com>
2021-01-15 17:56:11 -0500

    super garbage, still working through a effective way to extract the relevant data

--- data_extract.py
+++ data_extract.py
@@ -1,46 +1,71 @@
 import os
-from typing import IO, List, Union
+from typing import IO, List, Union, Dict
 import json
+
+PROVIDERS = [ 
+    'Alibaba.com Llc', 'Amazon', 'Azure', 'Baidu Inc', 'Digital Ocean', 'Google Cloud', 
+    'Kamatera, Inc', 'Linode', 'M247 Ltd', 'M247 Ltd New York Infrastructure',
+    'M247 Europe Srl', 'M247 Ltd Rome Network', 'M247 Los Angeles', 'M247',
+    'M247 Europe', 'M247 Ltd Paris Infrastructure', 'M247 Ltd Oslo Infrastructure',
+    'Oracle', 'Rackspace Hosting', 'Salesforce', 'Tencent Cloud Computing (Beijing) Co. Ltd',
+    'Verizon Communications', 'Vultr Holdings, LLC'
+    ]
 
 def list_files(dir: IO, ext: str) -> IO:
     return (f for f in os.listdir(dir) if f.endswith('.' + ext))
 
-def extract_data(file: IO) -> Union[str, str]:
-    mess = ['\n', '\t', ' ']
+def get_provider(file: IO) -> str:
     provider = ''
-    ranges = []
+    COMPANIES = []
     with open(file, "r") as file_ptr:
-        line = file_ptr.readlines()
-        for i in line:
-            try:
-                for junk in mess:
-                    if junk in i:
-                        i = i.replace(junk, ',')
-                i = i.split(',')
-                i = list(filter(None, i))
-                i.pop(0)
-                provider = ' '.join(i[3:5])
-                ips = ' '.join(i[0:3])
-                ranges.append(ips)
-            except (ValueError, IndexError):
-                continue
-    ranges.sort()
-    return(provider, ranges)
+        lines = file_ptr.readlines()
+        characters = ['\n', '\t', ' ']
+        for line in lines:
+            for char in characters:
+                if char in line:
+                    line = line.replace(char, ',')
+            line = list(filter(None, line.split(',')))
+            provider = line[4:len(line)]
+            provider = ' '.join(provider)
+            COMPANIES.append(provider)
+    print(COMPANIES)
 
-def convert_to_json(file: IO, key: str, val: List):
-    data_set = { key: val }
-    json_dump = json.dumps(data_set, sort_keys=True, indent=4)
-    file = file + ".json"
-    cwd = os.getcwd()
-    path = cwd + "/" + file
-    with open(path, "w") as file_ptr:
-        file_ptr.write(json_dump)
+# def extract_data(file: IO) -> Union[Dict, Dict]:
+#     mess = ['\n', '\t', ' ']
+#     provider = ""
+#     ip_ranges = {}
+#     ip_list = []
+#     ip_ranges['ip_ranges'] = []
+#     with open(file, "r") as file_ptr:
+#         line = file_ptr.readlines()
+#         for i in line:
+#             try:
+#                 for junk in mess:
+#                     if junk in i:
+#                         i = i.replace(junk, ',')
+#                 i = i.split(',')
+#                 i = list(filter(None, i))
+#                 i.pop(0)
+#                 provider = ' '.join(i[3:5])
+#                 ips = ' '.join(i[0:3])
+#                 ip_list.append(ips)
+#                 ip_list.sort()
+#                 ip_ranges['ip_ranges'] = ip_list
+#             except (ValueError, IndexError):
+#                 continue
+#     return(provider, ip_ranges)
+
+# def convert_to_json(file: IO, key: Dict, val: Dict):
+#     data_set = { key: { key: val } }
+#     json_dump = json.dumps(data_set, sort_keys=True, indent=4)
+#     file = file + ".json"
+#     cwd = os.getcwd()
+#     path = cwd + "/" + file
+#     with open(path, "w") as file_ptr:
+#         file_ptr.write(json_dump)
 
 if __name__ == "__main__":
     cwd = os.getcwd()
     files = list_files(cwd, "txt")
     for f in files:
-        key, val = extract_data(f)
-        f = f[:-4]
-        print(f)
-        convert_to_json(f, key, val)
+        get_provider(f)
